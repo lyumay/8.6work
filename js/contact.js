0e1e2e430ba73b34d8b900a65a33efd3abcc6ffe
lyumay <1241015526@qq.com>
2020-08-10 17:18:42 +0800

    柴梦昕 移动端

---
+++ js/contact.js
@@ -0,0 +1,17 @@
+$(function () {
+    var num = 0;
+    $(".nav").hammer().bind("click",function(){
+        num++;
+        if(num%2==1){
+            $(this).find("span").html("关闭");
+            $(".nav-mask").css({
+                display:"block"
+            })
+        }else if(num%2==0){
+            $(this).find("span").html("联系我们");
+            $(".nav-mask").css({
+                display:"none"
+            })
+        }
+    });
+})
